4f59a0080d672e8f1111883d37a8ccfd6cf6892e
Burgundysocks <ia05125@naver.com>
2024-08-04 19:42:42 +0900

    인덱스 기본틀

--- src/main/java/com/awspractice/book/controller/PageController.java
+++ src/main/java/com/awspractice/book/controller/PageController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/list")
     public String list() {
         return "post/list"; // Thymeleaf 템플릿 이름을 반환
